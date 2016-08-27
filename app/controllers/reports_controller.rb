a5eb41f5381e821adea8d447b7c15ca8bb2e6eb7
iceberg20 <italoberg@yahoo.com.br>
2016-08-27 17:31:36 -0300

    infected report percentage

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,15 @@
 class ReportsController < ApplicationController
+	def infected
+		survivors = Survivor.all
+		all = Survivor.count
+		qtd_infect = 0
+		survivors.each do |s|
+				if s.infected_reports == 3 
+					qtd_infect+= 1
+				end 
+		end
+		percent = ( qtd_infect.to_f/all.to_f ) * 100 
+
+		render json: { "infected_survivors":"#{percent}"+"%" }
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :survivors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   
+  get :infected, to: 'reports#infected'
+ 
 end
