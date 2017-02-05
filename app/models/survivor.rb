d9149c561402d1f694e14615dd0d0dcc351e5ff5
iceberg20 <italoberg@yahoo.com.br>
2017-02-05 04:14:54 -0300

    add validation to atributes

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -2,6 +2,11 @@ class Survivor < ApplicationRecord
 	has_one :inventory
 	ActionController::Parameters.permit_all_parameters = true
 
+	validates :name, :age, :gender, :lat, :long, presence: true
+
+	def initializer
+	end
+
 	def infected?
 		if self.infected_reports >= 3
 			true
