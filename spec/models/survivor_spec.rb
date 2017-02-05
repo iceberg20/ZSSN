75a8f6bb63be10bcf35fa1cb4d0d6cc1d5cdbdbe
iceberg20 <italoberg@yahoo.com.br>
2017-02-05 04:15:52 -0300

    test the atributes validation

--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -1,17 +1,43 @@
 require_relative '../spec_helper'
 
+RSpec.describe "Create Survivor", :type => :model do
+	context "missing" do
+	  it "name" do
+	  	survivor = Survivor.new(name:nil, age: 23, gender: "Male", lat: 3, long: 2, infected_reports: 0)
+	  	survivor.save
+	  	expect(survivor.new_record?).to eq(true)
+	  end
+
+	  it "age" do
+	  	survivor = Survivor.new(name: "jack", age: nil, gender: "Male", lat: 3, long: 2, infected_reports: 0)
+	  	survivor.save
+	  	expect(survivor.new_record?).to eq(true)
+	  end
+
+	  it "gender" do
+	  	survivor = Survivor.new(name: "jack", age: 23, gender: nil, lat: 3, long: 2, infected_reports: 0)
+	  	survivor.save
+	  	expect(survivor.new_record?).to eq(true)
+	  end
+
+	  it "lat" do
+	  	survivor = Survivor.new(name: "jack", age: 23, gender: "Male", lat: nil, long: 2, infected_reports: 0)
+	  	survivor.save
+	  	expect(survivor.new_record?).to eq(true)
+	  end
+
+	  it "long" do
+	  	survivor = Survivor.new(name: "jack", age: 23, gender: "Male", lat: 3, long: nil, infected_reports: 0)
+	  	survivor.save
+	  	expect(survivor.new_record?).to eq(true)
+	  end
+	end
+end
 
 RSpec.describe Survivor, :type => :model do
   it "is named Michonne" do
   	survivor = Survivor.new
   	survivor.name = 'Michonne'
   	expect(survivor.name).to eq('Michonne')
-  end
-end
-
-RSpec.describe Survivor, :type => :model do
-  it "is yelling hi" do
-  	survivor = Survivor.new
-  	expect(survivor.yell).to eq('Hi!')
   end
 end
