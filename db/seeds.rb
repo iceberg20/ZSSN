35e7da0af95654bfe70c8a8a0028814aa34bd45a
iceberg20 <italoberg@yahoo.com.br>
2017-02-07 09:58:10 -0300

    seed to populate the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = ["David", "Rick", "Daryl", "Glenn", "Norman", "Dixon", "Maggie", "Carl", "Michonne", "Carol", "Abraham", "Morgan"]
+
+names.each do |name|
+	survivor = Survivor.create(name: name , age: 23, gender: "Male", lat: 3, long: 2, infected_reports: 0)
+	survivor.inventory = Inventory.create(water: 1, food: 2, medication: 1, ammunition: 3)
+end
